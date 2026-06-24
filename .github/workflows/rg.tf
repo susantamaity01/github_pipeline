0362ada77a83f5dbbfc5458c335c3c21d5606839
susantamaity01 <susantamaity.rsgis@gmail.com>
2026-06-25 00:50:40 +0530

    Update rg.tf

--- .github/workflows/rg.tf
+++ .github/workflows/rg.tf
@@ -1,1 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.74.0"
+provider "azurerm" {
+   features {}
+}
 
+resource "azurerm_resource_group" "rg01" {
+  name = "github"
+  loaction = "centralindia"
+}
